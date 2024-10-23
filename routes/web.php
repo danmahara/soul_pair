1ad509a7f9cf46a359f5abbaac839cbac0ac3e0b
Dan Mahara <dan.mahara.0909@gmail.com>
2024-10-24 00:07:02 +0545

    added user table migration fields and create function to make slug

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('website.index');
 });
+
+Route::resource('users', UserController::class);
+
+
